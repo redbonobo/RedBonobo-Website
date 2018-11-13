b153abe9bd1bee07bb340693d618f5e8e30b6c2d
Marcel <44846634+redbonobo@users.noreply.github.com>
2018-11-13 22:25:27 +0100

    Start Files

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1>hello World</h1>; //babel will compile this into javascript
+ReactDOM.render(element, document.getElementById("root"));
